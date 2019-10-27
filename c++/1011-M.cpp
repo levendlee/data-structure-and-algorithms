f6b06b6327e37c71ac4a0b4b46294c0edcf23ce2
levendlee <levendlee@hotmail.com>
2019-10-27 12:02:19 -0400

    Leetcode 1011 in C++.

---
+++ c++/1011-M.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int D) {
+        int64_t lo = *std::max_element(weights.begin(), weights.end());
+        int64_t hi = std::accumulate(weights.begin(), weights.end(), static_cast<int64_t>(0));
+        int min_capacity = 0;
+        while (lo < hi) {
+            int64_t mid = lo + (hi - lo) / 2;
+            int64_t sum = 0;
+            int n_packages = 1;
+            for (const auto& w : weights) {
+                sum += w;
+                if (sum > mid) {
+                    ++n_packages;
+                    sum = w;
+                }
+            }
+            if (n_packages > D) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+
+        return lo;
+    }
+};
