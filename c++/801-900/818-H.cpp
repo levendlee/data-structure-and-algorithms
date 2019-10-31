aa552b38c03b9773533e97816a780a912d58bbe4
levendlee <levendlee@hotmail.com>
2019-10-30 22:23:41 -0400

    Leetcode 818 in C++.

---
+++ c++/801-900/818-H.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    int racecar(int target) {
+        if (target == 0) return 0;
+
+        std::vector<int> dp(target + 1, INT_MAX);
+        dp[0] = 0; dp[1] = 1;
+
+        int k = 1;
+        for (int i = 1; i <= target; ++i) {
+            if (i == (1 << k) - 1) {
+                dp[i] = k;
+                ++k;
+            } else {
+                // Move k-1 steps. Reset. Move back j steps. Move forward again.
+                for (int j = 0; j < k - 1; ++j) {
+                    dp[i] = std::min(dp[i], dp[i - (1 << (k - 1)) + (1 << j)] + k - 1 + j + 2);
+                }
+                // Move k steps. Reset. Move backward.
+                dp[i] = std::min(dp[i], dp[(1 << k) - 1 - i] + k + 1);
+            }
+        }
+
+        return dp[target];
+    }
+};
