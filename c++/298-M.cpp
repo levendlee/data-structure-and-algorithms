a7bc0dc1d7b29aeacdbce93c125b4dccd5e1823a
levendlee <levendlee@hotmail.com>
2019-12-14 15:46:56 -0500

    Leetcode 298 in C++.

---
+++ c++/298-M.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    int max_len_;
+
+public:
+    void findConsecutive(TreeNode* root, int pval, int plen) {
+        if (!root) return;
+        int cnt = (root->val == pval + 1) ? (plen + 1) : 1;
+        findConsecutive(root->left, root->val, cnt);
+        findConsecutive(root->right, root->val, cnt);
+        max_len_ = std::max(max_len_, cnt);
+    }
+
+    int longestConsecutive(TreeNode* root) {
+        if (!root) return 0;
+        max_len_ = 0;
+        findConsecutive(root, root->val, 1);
+        return max_len_;
+    }
+};
