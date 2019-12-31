c6eea8888cc1cee91d85b4a347ea75580a3ba7e7
levendlee <levendlee@hotmail.com>
2019-12-31 10:48:52 -0500

    Leetcode 2 in C++.

---
+++ c++/2-M.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* const preHead = new ListNode(0);
+        ListNode* node = preHead;
+        int carry = 0;
+        while (l1 || l2) {
+            int lhs = l1 ? l1->val : 0; if (l1) l1 = l1->next;
+            int rhs = l2 ? l2->val : 0; if (l2) l2 = l2->next;
+            int sum = lhs + rhs + carry;
+            node->next = new ListNode(sum % 10);
+            node = node->next;
+            carry = sum / 10;
+        }
+        if (carry) {
+            node->next = new ListNode(carry);
+        }
+        return preHead->next;
+    }
+};
