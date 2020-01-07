a4e04f2b4c3e2179cf2a42e8e12bcd073d5874f9
levendlee <levendlee@hotmail.com>
2020-01-06 23:37:39 -0500

    Leetcode 45,588 in C++.

---
+++ c++/45-H.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        const int n = nums.size();
+        if (n <= 1) return 0;
+        
+        int step = 0;
+        int cur = 0, bound = 0;
+        while (bound < n-1 ) {
+            int new_bound = bound;
+            for (int i = cur; i <= bound; ++i) {
+                new_bound = std::max(new_bound, i + nums[i]);
+            }
+            cur = bound + 1;
+            bound = new_bound;
+            ++step;
+        }
+        return step;
+    }
+};
---
+++ c++/588-H.cpp
@@ -0,0 +1,70 @@
+struct FSNode {
+    bool isfile;
+    string name;
+    string content;
+    map<string, FSNode*> subdir;
+
+    FSNode(const string& name) : isfile(false), name(name) {}
+
+    FSNode* getSubdir(const string& name) {
+        if (!subdir.count(name)) {
+            subdir[name] = new FSNode(name);
+        }
+        return subdir[name];
+    }
+    bool hasSubdir(const string& name) {
+        return subdir[name];
+    }
+};
+
+class FileSystem {
+private:
+    FSNode* root_;
+
+    FSNode* mkdir_and_cd(const string& path) {
+        const int n = path.size();
+        int idx = 1, next_idx;
+
+        auto node = root_;
+        while (idx < n) {
+            next_idx = path.find('/', idx);
+            //auto dir = path.substr(idx, next_idx - idx);
+            node = node->getSubdir(path.substr(idx, next_idx - idx));
+            if (next_idx == string::npos) break;
+            idx = next_idx + 1;
+        }
+        return node;
+    }
+
+public:
+    FileSystem() : root_(new FSNode("")) {
+
+    }
+
+    vector<string> ls(string path) {
+        auto node = mkdir_and_cd(path);
+        if (node->isfile) {
+            return {node->name};
+        }
+        vector<string> res;
+        for (const auto& p : node->subdir) {
+            res.push_back(p.first);
+        }
+        return res;
+    }
+
+    void mkdir(string path) {
+        mkdir_and_cd(path);
+    }
+
+    void addContentToFile(string filePath, string content) {
+        auto node = mkdir_and_cd(filePath);
+        node->isfile = true;
+        node->content += content;
+    }
+
+    string readContentFromFile(string filePath) {
+        auto node = mkdir_and_cd(filePath);
+        return node->content;
+    }
+};
