082101eed77b4bded06289ca1d48fde072f8a339
levendlee <levendlee@hotmail.com>
2020-01-11 17:33:25 -0500

    Leetcode 322 in C++.

---
+++ c++/322-M.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+            for (const auto& c : coins) {
+                if (i - c >= 0) {
+                    if (dp[i - c] == INT_MAX) continue;
+                    dp[i] = std::min(dp[i], dp[i - c] + 1);
+                }
+            }
+        }
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
+    }
+};
