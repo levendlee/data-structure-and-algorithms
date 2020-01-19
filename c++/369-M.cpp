d7eab01886748c30edebf6e606945cb7f0eb3a2d
levendlee <levendlee@hotmail.com>
2020-01-18 21:18:56 -0500

    Leetcode 369 in C++.

---
+++ c++/369-M.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseLinkedList(ListNode* head) {
+        if (!head || !head->next) return head;
+
+        ListNode* pre = nullptr;
+        ListNode* cur = head;
+        while (cur) {
+            auto nex = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = nex;
+        }
+
+        return pre;
+    }
+
+    ListNode* plusOne(ListNode* head) {
+        const auto reverse_head = reverseLinkedList(head);
+
+        auto node = reverse_head;
+        int carry = 1;
+        while (node) {
+            auto sum = node->val + carry;
+            node->val = sum % 10;
+            carry = sum / 10;
+            if (!carry) {
+                break;
+            }
+            if (!node->next) {
+                node->next = new ListNode(carry);
+                break;
+            } else {
+                node = node->next;
+            }
+        }
+
+        return reverseLinkedList(reverse_head);
+    }
+};
