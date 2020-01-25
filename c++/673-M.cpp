985a25c936d75621b323a281b863e6f95b238a34
levendlee <levendlee@hotmail.com>
2020-01-24 22:40:36 -0500

    Leetcode 673 with Segment Tree in C++.

--- c++/673-M.cpp
+++ c++/673-M.cpp
@@ -32,3 +32,87 @@ public:
         return total_max_cnt;
     }
 };
+
+struct Node {
+    int range_left;
+    int range_right;
+    pair<int, int> max_len_cnt;
+    Node* node_left;
+    Node* node_right;
+
+    Node(int left, int right) : range_left(left), range_right(right), node_left(nullptr), node_right(nullptr) {}
+    int range_mid() const {
+        if (range_left + 1 == range_right)
+            return range_left;
+        else
+            return (range_left + range_right) / 2;
+    }
+    Node* left() {
+        if (!node_left) node_left = new Node(range_left, range_mid());
+        return node_left;
+    }
+    Node* right() {
+        if (!node_right) node_right = new Node(range_mid() + 1, range_right);
+        return node_right;
+    }
+};
+
+
+// Segment Tree
+class Solution {
+private:
+    pair<int, int> merge(const pair<int, int>& lhs, const pair<int, int>& rhs) {
+        if (lhs.first == rhs.first) {
+            if (lhs.first == 0) {
+                return {0, 1};
+            } else {
+                return {lhs.first, lhs.second + rhs.second};
+            }
+        } else {
+            return std::max(lhs, rhs);
+        }
+    }
+
+    void update(Node* node, int key, const pair<int, int>& len_cnt) {
+        if (node->range_right >= key) {
+            if (node->range_left == node->range_right) {
+                node->max_len_cnt = merge(node->max_len_cnt, len_cnt);
+            } else {
+                if (node->range_mid() >= key && node->left()->range_right != node->range_right) {
+                    update(node->left(), key, len_cnt);
+                } else {
+                    update(node->right(), key, len_cnt);
+                }
+                node->max_len_cnt = merge(node->left()->max_len_cnt, node->right()->max_len_cnt);
+            }
+        }
+    }
+
+    pair<int, int> query(Node* node, int upper) {
+        if (node->range_right <= upper) {
+            return node->max_len_cnt;
+        } else if (node->range_left > upper) {
+            return {0, 1};
+        } else {
+            auto lhs = query(node->left(), upper);
+            auto rhs = query(node->right(), upper);
+            return merge(lhs, rhs);
+        }
+    }
+
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        const int n = nums.size();
+        if (n <= 1) return n;
+
+        const auto ss = *std::min_element(nums.begin(), nums.end());
+        const auto se = *std::max_element(nums.begin(), nums.end());
+        Node* node = new Node(ss, se);
+        for (int i = 0; i < n; ++i) {
+            auto res = query(node, nums[i] - 1);
+            ++res.first;
+            update(node, nums[i], res);
+        }
+        return node->max_len_cnt.second;
+    }
+};
