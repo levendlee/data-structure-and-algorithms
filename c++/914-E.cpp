451e87ec63d4b8d372310ec9596c502d692a73dd
levendlee <levendlee@hotmail.com>
2020-01-26 14:18:39 -0500

    Leetcode 914 in C++.

---
+++ c++/914-E.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool hasGroupsSizeX(vector<int>& deck) {
+        unordered_map<int, int> counter;
+        for (const auto& card : deck) {
+            ++counter[card];
+        }
+        int min_cnt = INT_MAX;
+        for (const auto& p: counter) {
+            min_cnt = std::min(min_cnt, p.second);
+        }
+        const auto can_split = [&](const int i) {
+            for (const auto& p : counter) {
+                if (p.second % i) {
+                    return false;
+                }
+            }
+            return true;
+        };
+
+        for (int i = 2; i <= min_cnt; ++i) {
+            if (can_split(i)) return true;
+        }
+        return false;
+    }
+};
