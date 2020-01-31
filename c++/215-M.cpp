e1f463492db9f6b90a24b36a15b2fd79b6e7ac50
levendlee <levendlee@hotmail.com>
2020-01-30 21:32:12 -0500

    Leetcode 215 in C++.

---
+++ c++/215-M.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq;
+        for (const auto& n : nums) {
+            pq.push(-n);
+            if (pq.size() > k) pq.pop();
+        }
+        return -pq.top();
+    }
+};
