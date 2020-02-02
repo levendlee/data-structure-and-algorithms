2782d80a8276a3842f0e0285c22b6cb6a81af60b
levendlee <levendlee@hotmail.com>
2020-02-01 20:04:49 -0500

    Leetcode 18 in C++.

---
+++ c++/18-M.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> res;
+        const int n = nums.size();
+        if (n < 4) return res;
+
+        std::sort(nums.begin(), nums.end());
+        for (int i = 0; i < n; ++i) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            for (int j = i + 1; j < n; ++j) {
+                if (j > i + 1 && nums[j] == nums[j-1]) continue;
+                int ntarget = target - (nums[i] + nums[j]);
+                int k = j + 1, l = n - 1;
+                while (k < l) {
+                    int sum = nums[k] + nums[l];
+                    if (sum == ntarget) {
+                        res.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        while (k < l && nums[k + 1] == nums[k]) ++k;
+                        ++k;
+                        while (k < l && nums[l - 1] == nums[l]) --l;
+                        --l;
+                    } else if (sum > ntarget) {
+                        while (k < l && nums[l - 1] == nums[l]) --l;
+                        --l;
+                    } else {
+                        while (k < l && nums[k + 1] == nums[k]) ++k;
+                        ++k;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
