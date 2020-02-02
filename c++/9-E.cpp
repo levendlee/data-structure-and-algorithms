da19f9b9bdb70a4300c540a3fe72632e6611fd84
levendlee <levendlee@hotmail.com>
2020-02-01 22:21:31 -0500

    Leetcode 9 in C++.

---
+++ c++/9-E.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        if (x == 0) return true;
+        /*
+        auto s = std::to_string(x);
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            if (s[i] != s[j]) return false;
+            ++i; --j;
+        }
+        */
+        if (x < 10) return true;
+
+        long base = 10;
+        while (base * 10 <= x) base *= 10;
+
+        while (base >= 1) {
+            if (x / base != x % 10) return false;
+            x = (x % base) / 10;
+            base = base / 100;
+        }
+
+        return true;
+    }
+};
