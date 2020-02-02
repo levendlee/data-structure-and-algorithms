f1560a7be4127c949f050414eab4d1ef43935605
levendlee <levendlee@hotmail.com>
2020-02-02 14:24:58 -0500

    Leetcode 137 in C++.

---
+++ c++/137-M.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    // Overall idea: 1) using XOR as 1 bit adder.
+    //               2) using AND as the carry calculator.
+    //               3) using mask and AND to mod the times
+
+    int singleNumber(vector<int>& nums) {
+        int x0 = 0, x1 = 0, mask;
+        for (const auto n : nums) {
+            x1 ^= (x0 & n); // the msb of the counter which counts the occurrence of 1s.
+            x0 ^= n;        // the lsb of the counter which counts the occurrence of 1s.
+            mask = ~(x1 & x0);  // the mask to reset to zero when the counter reaches 3
+            x1 &= mask;     // reset
+            x0 &= mask;     // reset
+        }
+        return x0;
+    }
+};
