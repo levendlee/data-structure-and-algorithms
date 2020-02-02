b2ae332066481e3961734366638b0b94ba7f3a95
levendlee <levendlee@hotmail.com>
2020-02-02 16:54:53 -0500

    Leetcode 144 in C++.

---
+++ c++/144-M.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+
+        stack<TreeNode*> stk;
+        TreeNode* node = root;
+
+        while (!stk.empty() || node) {
+            if (node) {
+                res.push_back(node->val);
+                stk.push(node->right);
+                node = node->left;
+            } else {
+                node = stk.top(); stk.pop();
+            }
+        }
+
+        return res;
+    }
+};
