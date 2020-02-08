a68809b6702b1f8639146c8aaffa223fee94fb97
levendlee <levendlee@hotmail.com>
2020-02-08 14:50:21 -0500

    Leetcode 670 in C++.

--- c++/670-M.cpp
+++ c++/670-M.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int maximumSwap(int num) {
         auto str = to_string(num);
+        /*
         auto max_str = str;
 
         const int n = str.size();
@@ -25,6 +26,22 @@ public:
             ++i;
         }
         std::swap(str[swap1], str[swap2]);
+        return stoi(str);
+        */
+        const int n = str.size();
+        int last[10] = {-1};
+        for (int i = 0; i < n; ++i) {
+            last[str[i] - '0'] = i;
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int d = 9; d > str[i] - '0'; --d) {
+                if (last[d] > i) {
+                    std::swap(str[last[d]], str[i]);
+                    goto result;
+                }
+            }
+        }
+        result:
         return stoi(str);
     }
 };
