f9c8e09d0183d7023f894e30d70ced6966f1ac2f
levendlee <levendlee@hotmail.com>
2020-02-08 00:05:34 -0500

    Leetcode 157 in C++.

---
+++ c++/157-E.cpp
@@ -0,0 +1,31 @@
+// Forward declaration of the read4 API.
+int read4(char *buf);
+
+class Solution {
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    int read(char *buf, int n) {
+        int nchar = 0;
+        char tmp[4];
+        while (nchar < n) {
+            int rc;
+            if (n - nchar >= 4) {
+                rc = read4(buf);
+            } else {
+                rc = read4(tmp);
+                rc = std::min(rc, n - nchar);
+                for (int k = 0; k < rc; ++k) {
+                    buf[k] = tmp[k];
+                }
+            }
+            nchar += rc;
+            buf += rc;
+            if (rc != 4) return nchar;
+        }
+        return nchar;
+    }
+};
