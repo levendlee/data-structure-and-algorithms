0bbc131d0dbe56c62604389658a0dde9f0a754a1
levendlee <levendlee@hotmail.com>
2020-02-07 21:36:49 -0500

    Leetcode 938 in C++.

---
+++ c++/938-E.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    int inOrder(TreeNode* root, int L, int R) {
+        if (!root) return 0;
+        int sum = root->val >= L && root->val <= R ? root->val : 0;
+        if (root->val > L) sum += inOrder(root->left, L, R);
+        if (root->val < R) sum += inOrder(root->right, L, R);
+        return sum;
+    }
+
+public:
+    int rangeSumBST(TreeNode* root, int L, int R) {
+        return inOrder(root, L, R);
+    }
+};
