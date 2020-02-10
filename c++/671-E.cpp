b879eb42d1cc7527d031691b0e3d2833f83bfe26
levendlee <levendlee@hotmail.com>
2020-02-09 19:35:50 -0800

    Leetcode 671 in C++.

---
+++ c++/671-E.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    int min_;
+    int second_min_;
+    bool second_min_set_;
+
+    void preOrder(TreeNode* root) {
+        if (!root) return;
+        if (root->val != min_) {
+            second_min_set_ = true;
+            second_min_ = std::min(second_min_, root->val);
+            return;
+        }
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+
+public:
+    int findSecondMinimumValue(TreeNode* root) {
+        if (!root) return -1;
+        min_ = root->val;
+        second_min_ = INT_MAX;
+        second_min_set_ = false;
+        preOrder(root);
+        return !second_min_set_ ? -1 : second_min_;
+    }
+};
